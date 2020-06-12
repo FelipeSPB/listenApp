1a3d3ca5bb0d65c78a5b3b6fba046e63907c9f95
FelipeSPB <felipebiancalana@outlook.com>
2020-06-12 01:56:40 -0300

    Id's Colocados nos Botões e Arquivos da Profile Fragment; Fragment configurada; funcao listener feita, função findViews feita; Respostas já colocadas;

--- app/src/main/java/com/example/listenapp/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/listenapp/fragment/ProfileFragment.java
@@ -1,6 +1,8 @@
 package com.example.listenapp.fragment;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,10 +10,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.listenapp.R;
+import com.example.listenapp.main.LoginActivity;
+import com.example.listenapp.main.MainActivity;
 
 public class ProfileFragment extends Fragment {
+
+    Context context;
+    View view;
+    ImageView userPicture;
+    TextView userName, quantityPlaylists;
+    Button suggestedSongs, editProfile, friendsList, aboutApp, signOut;
+
 
     public ProfileFragment() {
     }
@@ -19,11 +34,71 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       return inflater.inflate(R.layout.fragment_profile, container, false);
+        view = container;
+        return inflater.inflate(R.layout.fragment_profile, container, false);
 
     }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        this.context = context;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        findViews();
+        setListeners();
+    }
+
+
+    private void findViews(){
+        userPicture = view.findViewById(R.id.user_picture);
+        userName = view.findViewById(R.id.user_name);
+        quantityPlaylists = view.findViewById(R.id.quantity_playlists);
+        suggestedSongs = view.findViewById(R.id.suggested_songs_btn);
+        editProfile = view.findViewById(R.id.edit_profile_btn);
+        friendsList = view.findViewById(R.id.friends_list_btn);
+        aboutApp = view.findViewById(R.id.about_btn);
+        signOut = view.findViewById(R.id.sign_out_btn);
+    }
+
+    private void setListeners(){
+        String textPatternButton = "A implementar";
+        suggestedSongs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, textPatternButton, Toast.LENGTH_SHORT).show();
+            }
+        });
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, textPatternButton, Toast.LENGTH_SHORT).show();
+            }
+        });
+        friendsList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, textPatternButton, Toast.LENGTH_SHORT).show();
+            }
+        });
+        aboutApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, textPatternButton, Toast.LENGTH_SHORT).show();
+            }
+        });
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Activity activity = (Activity) view.getContext();
+                activity.startActivity(new Intent(activity, MainActivity.class));
+            }
+        });
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/listenapp/viewmodel/ViewModelNews.kt
+++ app/src/main/java/com/example/listenapp/viewmodel/ViewModelNews.kt
@@ -4,23 +4,14 @@ import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
 import com.example.listenapp.repository.RepositoryVagalume
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 
 class ViewModelNews(application: Application) : AndroidViewModel(application){
 
     val NewsSet = MutableLiveData<String>()
     private val repository =  RepositoryVagalume()
 
-    fun getWeather(lat: String, lon: String) = CoroutineScope(IO).launch {
-        repoWeather.getWeather(lat, lon).let { resposta ->
-            stringFormatada.postValue(
-                    "País: " + resposta.sys?.country + "\n" +
-                            "Temp: " + resposta.main?.temp + "\n" +
-                            "Temp(Min): " + resposta.main?.temp_min + "\n" +
-                            "Temp(Max): " + resposta.main?.temp_max + "\n" +
-                            "Humildade: " + resposta.main?.humidity + "\n" +
-                            "Pressão: " + resposta.main?.pressure
-            )
-        }
-    }
 
 }
